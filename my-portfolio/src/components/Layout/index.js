519bfe7c36fe76ca0226af31c4275f34834f2dad
Mariya Setko <mariya.setko@gmail.com>
2022-07-28 09:21:20 -0400

    layout

--- my-portfolio/src/components/Layout/index.js
+++ my-portfolio/src/components/Layout/index.js
@@ -3,9 +3,19 @@ import './index.scss';
 
 const Layout = () => {
     return (
-      <>
+      <div className="App">
         <Sidebar />
-      </>
+        <div className='page'>
+          <span className='tags top-tags'>&lt;body&gt;</span>
+
+          <Outlet />
+
+          <span className='tags bottom-tags'>&lt;body&gt;
+          <br />
+          </span>
+          <span className='bottom-tag-html'>&lt;body&gt;</span>
+        </div>
+      </div>
     )
 }
 
--- my-portfolio/src/components/Sidebar/index.js
+++ my-portfolio/src/components/Sidebar/index.js
@@ -21,6 +21,19 @@ const Sidebar = () => (
                 <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
             </NavLink>
         </nav>
+        <ul>
+            <li>
+                <a target="_blank" rel='noreferrer' href="https://www.linkedin.com/in/mariyasetko/">
+                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                </a>
+            </li>
+
+            <li>
+                <a target="_blank" rel='noreferrer' href="https://github.com/mariyasetko">
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a>
+            </li>
+        </ul>
     </div>
 )
 
